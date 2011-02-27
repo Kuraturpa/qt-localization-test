2e69a2a970282fa2433a2749cf9ad6995da4ef72
Joonas <joo.ruot@gmail.com>
2011-02-27 11:26:42 +0200

    added translation files to project

---
+++ lorem.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="dialog.ui" line="16"/>
+        <source>Dialog</source>
+        <translation>Quus</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="mainwindow.ui" line="31"/>
+        <source>Qt-localization-test</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="47"/>
+        <source>Kieli</source>
+        <translation>Lorem</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="62"/>
+        <source>2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="65"/>
+        <source>lorem ipsum</source>
+        <translation>lorem ipsum</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <source>1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <location filename="mainwindow.ui" line="198"/>
+        <source>English</source>
+        <translation>del av englant</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="82"/>
+        <source>0</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="85"/>
+        <location filename="mainwindow.ui" line="190"/>
+        <source>Suomi</source>
+        <translation>pasa finljandii</translation>
+    </message>
+    <message utf8="true">
+        <location filename="mainwindow.ui" line="116"/>
+        <source>Tämän ohjelman avulla testataan qt localisointia</source>
+        <translation>Dolor sit ameth ar glot</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="131"/>
+        <source>Avaa ikkuna</source>
+        <translation>Truu perthu</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="154"/>
+        <source>Lopeta</source>
+        <translation>Suk</translation>
+    </message>
+</context>
+</TS>
